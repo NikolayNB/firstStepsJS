a2fa07c7c2bc9513bfb43baee76801800ce63248
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-06-17 22:43:29 +0200

    Completed course 1 of 5 Introduction to the language

---
+++ buenasPracticas.js
@@ -0,0 +1,12 @@
+let nombreEmpleado = "Jose Perez";
+let NombreEmpleado = "Pedro Ramirez";
+
+let edadEmpleado = 40;
+console.log(edadEmpleado);
+edadEmpleado = "Cuarenta";
+console.log(edadEmpleado);
+
+
+
+console.log(nombreEmpleado);
+console.log(NombreEmpleado);
